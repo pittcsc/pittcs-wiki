2171efba0279d53840c7b1d193978428484d637f
James Widmer <jow257@pitt.edu>
2026-07-10 08:17:52 -0400

    removes the except index.md part of redirects

--- next.config.js
+++ next.config.js
@@ -37,7 +37,7 @@ const nextConfig = {
   async redirects() {
     return [
       {
-        source: "/:slug*/:file((?<!index)\\S+).md(\\w*)",
+        source: "/:slug*/:file(\\S+).md(\\w*)",
         destination: "/:slug*/:file",
         permanent: true,
       },
